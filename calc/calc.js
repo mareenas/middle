ae589c1a870b6ab623de8c303cc702598e7c3ef5
mareenas <persempre1@yandex.ru>
2018-11-19 13:13:37 +0300

    objects 3

--- calc/calc.js
+++ calc/calc.js
@@ -1,40 +1,51 @@
+function Calculator(elId) {
+    this.num1Value = 0;
+    this.num2Value = 0;
+    this.resultField = undefined;
+
+    this.init = function () {
+        let el = document.getElementById(elId);
+        this.resultField = el.querySelector(".result-number");
+        let num1 = el.querySelector(".num1");
+        let num2 = el.querySelector(".num2");
+
+        let buttonSum = el.querySelector(".sum");
+        let buttonMinus = el.querySelector(".minus");
+        let buttonMultiply = el.querySelector(".multiply");
+        let buttonDivide = el.querySelector(".divide");
+
+        num1.addEventListener('change', this.onNum1Change.bind(this));
+        num2.addEventListener('change', this.onNum2Change.bind(this));
+
+        buttonSum.addEventListener('click', this.onButtonSumClick.bind(this));
+        buttonMinus.addEventListener('click', this.onButtonMinusClick.bind(this));
+        buttonMultiply.addEventListener('click', this.onButtonMultiplyClick.bind(this));
+        buttonDivide.addEventListener('click', this.onButtonDivideClick.bind(this));
+    };
+
+    this.onNum1Change = function(e) {
+        this.num1Value = Number(e.currentTarget.value);
+    };
+    this.onNum2Change = function(e) {
+        this.num2Value = Number(e.currentTarget.value);
+    };
+    this.onButtonSumClick = function () {
+        this.resultField.textContent = sum(this.num1Value, this.num2Value);
+    };
+    this.onButtonMinusClick = function () {
+        this.resultField.textContent = minus(this.num1Value, this.num2Value);
+    };
+    this.onButtonMultiplyClick = function () {
+        this.resultField.textContent = multiply(this.num1Value, this.num2Value);
+    };
+    this.onButtonDivideClick = function() {
+        this.resultField.textContent = divide(this.num1Value, this.num2Value);
+    };
+}
+
 let calcFactory = {
     getNewCalc: function(elId) {
-        let newCalc = {
-            num1Value: 0,
-            num2Value: 0,
-            init: function () {
-                let el = document.getElementById(elId);
-                let num1 = el.querySelector(".num1");
-                let num2 = el.querySelector(".num2");
-                let resultField = el.querySelector(".result-number");
-
-                let buttonSum = el.querySelector(".sum");
-                let buttonMinus = el.querySelector(".minus");
-                let buttonMultiply = el.querySelector(".multiply");
-                let buttonDivide = el.querySelector(".divide");
-
-                let that = this;
-                num1.addEventListener('change', function () {
-                    that.num1Value = Number(this.value);
-                });
-                num2.addEventListener('change', function () {
-                    that.num2Value = Number(this.value);
-                });
-                buttonSum.addEventListener('click', function () {
-                    resultField.textContent = sum(that.num1Value, that.num2Value);
-                });
-                buttonMinus.addEventListener('click', function() {
-                    resultField.textContent = minus(that.num1Value, that.num2Value);
-                });
-                buttonMultiply.addEventListener('click', function() {
-                    resultField.textContent = multiply(that.num1Value, that.num2Value);
-                });
-                buttonDivide.addEventListener('click', function() {
-                    resultField.textContent = divide(that.num1Value, that.num2Value);
-                });
-            }
-        };
+        let newCalc = new Calculator(elId);
         return newCalc;
     }
 };
